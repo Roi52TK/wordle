202dcc6ab2d066e58286a1f2864ee61b80de582d
Roi52TK <roitheknight52@gmail.com>
2026-09-16 13:40:14 +0300

    Created a dynamic keyboard UI.

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ const gameState = {
     gameOver: false
 };
 
+const keys = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACKSPACE"]
+]
+
 const board = document.getElementById("game-board");
+const keyboard = document.getElementById("keyboard");
 
 function createBoard() {
     for (let i = 0; i < gameSettings.maxTries; i++) {
@@ -25,4 +32,18 @@ function createBoard() {
     }
 }
 
+function createKeyboard() {
+    for(let keyRow = 0; keyRow < keys.length; keyRow++) {
+        const row = document.createElement("div");
+        row.className = "keyboard-row";
+        for(let key = 0; key < keys[keyRow].length; key++) {
+            const keyBtn = document.createElement("button");
+            keyBtn.textContent = keys[keyRow][key];
+            row.appendChild(keyBtn);
+        }
+        keyboard.appendChild(row);
+    }
+}
+
 createBoard();
+createKeyboard();
